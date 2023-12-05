883de2739b19d1bba569d802002453c036b16e8d
Ankitaghorpade2002 <ankitaghorpade012@gmail.com>
2023-12-05 21:54:43 +0530

    initial commit

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,58 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await categoryRepository.CreateAsync(category);
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await categoryRepository.GetAllAsync();
+            var response = new List<CategoryDto>();
+            foreach (var category in categories) 
+            {
+                response.Add(new CategoryDto
+                {
+                    Id = category.Id,
+                    Name = category.Name,
+                    UrlHandle = category.UrlHandle,
+                });
+            }
+            return Ok(response);
+        }
+    }
+}
